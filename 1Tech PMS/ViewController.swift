27880ace6b2087ea6b53336918420b5af940c73c
Bhavesh Gupta <bhavesh.bansal@outlook.com>
2023-07-29 08:13:05 +0530

    Initial Commit

---
+++ 1Tech PMS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1Tech PMS
+//
+//  Created by Bhavesh Gupta on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
